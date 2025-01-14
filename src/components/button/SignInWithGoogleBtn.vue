fe75c22728568e1c8a4d6368f1a5a093b77c1f7f
tonnyBryan <126952328+tonnyBryan@users.noreply.github.com>
2025-01-14 12:08:25 +0300

    google auth
    se connecter avec google

---
+++ src/components/button/SignInWithGoogleBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <button type="button" class="btn btn-light btn-lg d-flex align-items-center shadow-sm border rounded-pill px-4 py-2">
+    <img src="/assets/logo/google-logo.png" alt="Google Logo" class="me-2" width="24" height="24">
+    <span class="">Sign in with Google</span>
+  </button>
+</template>
+
+<style scoped>
+button {
+  width: 100%;
+  border-radius: 6px !important;
+  justify-content: center;
+  background-color: transparent;
+  color: #f3f3f3;
+  font-weight: 700 !important;
+}
+
+button:hover {
+  background-color: transparent !important;
+  color: #f3f3f3;
+}
+</style>
